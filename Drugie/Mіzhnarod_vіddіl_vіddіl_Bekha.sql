a20f61e7d1c5e1ca48897ef7bd62fa13f55654fc
Viktoriia Radosh <viktoriyaradosh@gmail.com>
2017-04-08 12:23:50 +0300

    Added Drugie/Mіzhnarod_vіddіl_vіddіl_Bekha.sql
    new query

---
+++ Drugie/Mіzhnarod_vіddіl_vіddіl_Bekha.sql
@@ -0,0 +1,13 @@
+USE RatingsKNU;
+GO
+
+create table Pozicz³ya_Un³versitetu_v_reytingu_Webometrics (
+	VNZ_krain_Skh³dnoi_Evropi int,
+	VNZ_krain_sv³tu int
+);
+
+create table Reyting_Un³versitetu_v_QS (
+	All_World int,
+	Eastern_Europe_and_Central_Asia int
+);
+
